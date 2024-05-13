ebcaf0993aeaad8339a1f0d7a20990aa2d1e9719
samuel.althaus <samuel.althaus@ECO3042>
2024-05-13 14:38:59 +0200

    feat: Agregados Repositorios, Servicios y clase de encriptación. Modificados modelos.

---
+++ TODO_V2.Server/Repository/Impl/ChoreRepository.cs
@@ -0,0 +1,71 @@
+﻿using TODO_V2.Server.Repository.Interfaces;
+using TODO_V2.Shared;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using TODO_V2.Shared.Models;
+
+namespace TODO_V2.Server.Repository.Impl
+{
+    public class ChoreRepository : IChoreRepository
+    {
+        private readonly IConfiguration _configuration;
+        private string ConnectionString => _configuration.GetConnectionString("TODO_V2DB");
+
+        public ChoreRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Add(Chore chore)
+        {        
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                //string query = @$"INSERT INTO Chore (Name, Descripcion, Finalizado) VALUES ('{chore.Name}', '{chore.Descripcion}', {(chore.Finalizado ? 1 : 0)});";
+                //dbConnection.Query(query);
+            }
+        }
+
+        public IEnumerable<Chore> GetAll()
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = @"SELECT * FROM Chore;";
+                return dbConnection.Query<Chore>(query);
+            }
+        }
+
+        public Chore GetById(int id)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = $"SELECT * FROM Chore WHERE Id = {id};";
+                return dbConnection.QueryFirstOrDefault<Chore>(query);
+            }
+        }
+      
+        public void Remove(int id)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = $"DELETE FROM Chore WHERE Id = {id};";
+                dbConnection.Execute(query);
+            }
+        }
+
+
+        public void Update(Chore chore)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                //string query = @$"UPDATE Chore SET Name = '{chore.Name}', Descripcion = '{chore.Descripcion}', Finalizado = '{(chore.Finalizado ? 1 : 0)}' WHERE Id = '{chore.Id}';";
+                //dbConnection.Execute(query);
+            }
+        }
+
+        public void LogicRemove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TODO_V2.Server/Repository/Impl/UserRepository.cs
@@ -0,0 +1,103 @@
+﻿using TODO_V2.Server.Repository.Interfaces;
+using TODO_V2.Shared;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using TODO_V2.Shared.Models;
+
+namespace TODO_V2.Server.Repository.Impl
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IConfiguration _configuration;
+        private string ConnectionString => _configuration.GetConnectionString("TODO_V2DB");
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public void Add(User user)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                try { 
+                string query = @$"INSERT INTO Usuarios (UserName, Password, Name, UserType) 
+                                VALUES ('{user.UserName}', '{user.Password}', '{user.Name}', '{user.UserType}');";
+                dbConnection.Execute(query);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public IEnumerable<User> GetAllAdmin()
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = @"SELECT * FROM Usuarios;";
+
+                return dbConnection.Query<User>(query);
+            }
+        }
+
+
+        public IEnumerable<User> GetAll()
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = @"SELECT * FROM Usuarios WHERE Deleted = 0;";
+
+                return dbConnection.Query<User>(query);
+            }
+        }
+
+        public User GetById(int id)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = $"SELECT * FROM Usuarios WHERE Id = {id} AND Deleted = 0;";
+
+                User aux = dbConnection.QuerySingle<User>(query);
+                User user = aux;
+                return user;
+            }
+        }
+
+
+        public void Remove(int id)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = $"DELETE FROM Usuarios WHERE Id = {id};";
+
+                dbConnection.Execute(query);
+            }
+        }
+
+        public void LogicRemove(int id)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = $"UPDATE Usuarios SET Deleted = 1 WHERE Id = {id};";
+
+                dbConnection.Execute(query);
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
+            {
+                string query = @$"UPDATE Usuarios SET Username = '{user.UserName}', Password ='{user.Password}', Name = '{user.Name}', UserType = '{user.UserType}' WHERE Id = '{user.Id}';";
+
+                dbConnection.Execute(query);
+            }
+        }        
+    }
+}
